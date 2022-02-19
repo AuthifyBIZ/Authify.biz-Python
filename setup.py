c070eb54d03368166e2f39d383ba05cc0d43c73d
Clynt <73389332+clynt707@users.noreply.github.com>
2022-02-19 22:15:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Package for the Authify.biz API'
 
 setup(
